5a670c01d633e6883df1b24489e55eb507416160
jay menorca <zaimenorca@gmail.com>
2023-10-18 15:33:17 -0400

    locators practice, e2e smoketest of login func practice site

--- src/com/jay/seleniumTraining/RahulAcademySiteLocatorsTest.java
+++ src/com/jay/seleniumTraining/RahulAcademySiteLocatorsTest.java
@@ -5,10 +5,11 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class RahulAcademySiteLocatorsTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\jay\\installed\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -16,22 +17,69 @@ public class RahulAcademySiteLocatorsTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 		driver.get("https://rahulshettyacademy.com/locatorspractice");
-		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
-		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
-		driver.findElement(By.className("signInBtn")).click();
-		
-		
-		String errorTxt = driver.findElement(By.cssSelector("p.error")).getText();
-		
-		
 		String temp = driver.getTitle();
 		System.out.println(temp);
+
 		temp = driver.getCurrentUrl();
 		System.out.println(temp);
+
+		//login error
+		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+
+		String errorTxt = driver.findElement(By.cssSelector("p.error")).getText();
 		System.out.println(errorTxt);
 		
-//		driver.close();
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Rahul");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@yahoo.com");
+		
+//		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("Rahul@yahoo.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("18008888888");
+		//driver.findElement(By.xpath("//form/div/button[2]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		
+		
+		temp = driver.findElement(By.cssSelector("form p.infoMsg")).getText();
+		System.out.println(temp);
+
+		String pwd = getPwd(temp);
+		
+
+		//back to login page to submit correct login
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+		
+		
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys(pwd);
+
+		Thread.sleep(1000);
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.id("chkboxTwo")).click();
+		
+		//driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+		driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+		
+		Thread.sleep(1000);
+		temp = driver.findElement(By.tagName("p")).getText();
+		System.out.println(temp);
+		Assert.assertEquals(temp, "You are successfully logged in.");
+		
+		String name = "rahul";
+		temp = driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText();
+		Assert.assertEquals(temp, "Hello " + name +",");
+		
+		driver.findElement(By.xpath("//*[text()='Log Out']")).click();
+		driver.close();
 //		driver.quit();
+	}
+
+	private static String getPwd(String temp) {
+		String[] pwdArr = temp.split("'");
+		String[] pwd = pwdArr[1].split("'");
+		return pwd[0];
 	}
 
 }
