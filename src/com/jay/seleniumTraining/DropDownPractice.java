d9fbd22e1971c8d65d6adadf3cc9b84a6c544eb5
jay menorca <zaimenorca@gmail.com>
2023-10-27 08:44:04 -0400

    SelJB2A - S55-S61

--- src/com/jay/seleniumTraining/DropDownPractice.java
+++ src/com/jay/seleniumTraining/DropDownPractice.java
@@ -1,6 +1,7 @@
 package com.jay.seleniumTraining;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -41,6 +42,52 @@ public class DropDownPractice {
 		driver.findElement(By.id("hrefIncChd")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.id("btnclosepaxoption")).click();
+		
+		
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		//driver.findElement(By.xpath("//a[contains(text(),'Chennai (MAA)')]")).click();
+		driver.findElement(By.xpath("//a[@value='MAA']")).click();
+		//driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1_CTXT")).click();
+		Thread.sleep(2000);
+		
+//		//via dynamic, using index
+//		driver.findElement(By.xpath("(//a[@value='BLR'])[2]")).click();
+		
+		//via parent child xpath
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BLR']")).click();
+		
+		//auto suggest dropdown
+		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		Thread.sleep(2000);
+		
+		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		for (WebElement option : options) {
+			if (option.getText().equalsIgnoreCase("India")) {
+				option.click();break;
+			}
+		}
+		
+//		//click on checkboxes
+//		WebElement fnf = driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily"));
+//		if (fnf.isSelected()==false) {
+//			fnf.click();
+//		}
+//		
+		//iterate and click on all chkboxes
+		//note: below doesn't work as there's conditions for the other chkboxes to be clickable
+//		List<WebElement> chkboxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
+//		for (WebElement chxbx : chkboxes) {
+//			if (chxbx.isSelected()==false) {
+//				chxbx.click();
+//			}
+//		}
+		
+		//iterate on all chkboxes and do assertions
+		//note: below doesn't work as there's conditions for the other chkboxes to be clickable
+		List<WebElement> chkboxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
+		for (WebElement chxbx : chkboxes) {
+			Assert.assertEquals(chxbx.isSelected(), false);
+		}		
 	}
 
 }
